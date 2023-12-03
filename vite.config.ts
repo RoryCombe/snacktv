1b9e9127c303e6a51307fda4cca214b224d8156f
Rory Combe <rory@combe.io>
2023-12-03 21:37:01 +0100

    Edited config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
   },
 });
